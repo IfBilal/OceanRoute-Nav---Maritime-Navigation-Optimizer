a9fc3cb9dcc8803787c9d5dfe7fb1e8de788df50
IfBilal <muhammadbilaltahir06@gmail.com>
2025-11-18 10:20:56 +0500

    vector class implementation

---
+++ Vector.h
@@ -0,0 +1,121 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <stdexcept>
+template <typename T>
+class vector
+{
+private:
+    T *arr;
+    int capacity;
+    int size;
+
+public:
+    vector()
+    {
+        size = 0;
+        capacity = 1;
+        arr = new T[capacity];
+    }
+
+    vector(const vector<T> &other)
+    {
+        size = other.size;
+        capacity = other.capacity;
+        arr = new T[capacity];
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = other.arr[i];
+        }
+    }
+
+    vector<T> &operator=(const vector<T> &other)
+    {
+        if (this != &other)
+        {
+            delete[] arr;
+            size = other.size;
+            capacity = other.capacity;
+            arr = new T[capacity];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = other.arr[i];
+            }
+        }
+        return *this;
+    }
+
+    void push_back(T value)
+    {
+        if (size == capacity)
+        {
+            capacity *= 2;
+            T *newArr = new T[capacity];
+            for (int i = 0; i < size; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = newArr;
+        }
+        arr[size] = value;
+        size++;
+    }
+
+    T &operator[](int index)
+    {
+        if (index >= size || index < 0)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        return arr[index];
+    }
+
+    void pop_back()
+    {
+        if (size == 0)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        size--;
+    }
+
+    int getSize() const
+    {
+        return size;
+    }
+
+    T &back()
+    {
+        if (size == 0)
+            throw std::out_of_range("Vector is empty");
+        return arr[size - 1];
+    }
+
+    bool empty() const
+    {
+        return size == 0;
+    }
+
+    int getCapacity() const
+    {
+        return capacity;
+    }
+
+    void clear()
+    {
+        size = 0;
+        if (capacity > 1)
+        {
+            capacity = 1;
+            T *newArr = new T[capacity];
+            delete[] arr;
+            arr = newArr;
+        }
+    }
+    ~vector()
+    {
+        delete[] arr;
+    }
+};
+
+#endif
